541362f2bfd3f4dae2889aa33afdd6f941982976
ThakareSupriya <mudesupriya53@gmail.com>
2023-01-20 15:08:11 +0530

    Created Master Class

---
+++ Mater/src/masterPkg/MasterClass.java
@@ -0,0 +1,10 @@
+package masterPkg;
+
+public class MasterClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is Master Class");
+	}
+
+}
