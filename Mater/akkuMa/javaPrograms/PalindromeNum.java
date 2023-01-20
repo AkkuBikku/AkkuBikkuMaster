30f1d04f9808fb63abbfb64048421d898c1a21ec
HP <HP@DESKTOP-BTQ2T4D.lan>
2023-01-20 15:55:04 +0530

    akkuMa source folder created by Akku

---
+++ Mater/akkuMa/javaPrograms/PalindromeNum.java
@@ -0,0 +1,12 @@
+package javaPrograms;
+
+public class PalindromeNum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("we will write a palindrome number program here");
+
+	}
+
+}
